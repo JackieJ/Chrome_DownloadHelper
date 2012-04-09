43994e0096dbc17c2c6f817c1dd22534500cfa9e
Jackie Jin <jacky@cloud30.com>
2012-04-08 22:51:21 -0700

    html and probe fix

--- chrome_download_helper/webRequestGetter/mediaRequestProbe.js
+++ chrome_download_helper/webRequestGetter/mediaRequestProbe.js
@@ -1,37 +1,3 @@
-/*========================NACL Client=============================*/
-DownloadHelperModule = null;//initialization                                                                                                                 statusText = 'NO-STATUS';
-
-var url = "www.test.com";
-
-var moduleDidLoad = function() {
-    DownloadHelperModule = document.getElementById('chrome_download_helper');
-    updateStatus('SUCCESS');
-    DownloadHelperModule.postMessage(url);
-};
-
-var handleMessage = function(message_event) {
-    var vidList = document.getElementById("vidList");
-    var li = document.createElement("li");
-    li.textContent = message_event.data;
-    vidList.appendChild(li);
-};
-
-var pageDidLoad = function() {
-    if (DownloadHelperModule == null) {
-        updateStatus('LOADING...');
-    } else {
-        updateStatus();
-    }
-};
-
-var updateStatus = function(opt_message) {
-    if (opt_message)
-        statusText = opt_message;
-    var statusField = document.getElementById('status_field');
-    if (statusField) {
-        statusField.innerHTML = statusText;
-    }
-}
 
 /*=====================Extension API==========================*/
 //save media links
@@ -66,6 +32,7 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     },
     {urls: ["<all_urls>"]});
 
+console.log("HI!!!!!!!");
 
 /*chrome.webRequest.onCompleted.addListener(function(details) {
 	console.log(JSON.stringify(details.url));
