9e6a020e27d2dd4e2d8ec57ab7daac57d8ecd6e7
Jackie Jin <jacky@cloud30.com>
2012-04-10 00:31:13 -0700

    major fix on activated tavs

--- chrome_download_helper/webRequestGetter/mediaRequestProbe.js
+++ chrome_download_helper/webRequestGetter/mediaRequestProbe.js
@@ -1,4 +1,3 @@
-
 /*=====================Extension API==========================*/
 //save media links
 var medialinks = [];
@@ -6,40 +5,34 @@ var medialinksMap = {};
 //media file extensions regex
 var mediaPattern = /\.rmvb|\.mpg|\.mpeg|\.avi|\.rm|\.wmv|\.mov|\.flv|\.mpg3|\.mp4|\.mp3/;
 
-//debugging functions/vars
+//display current tab info 
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	chrome.tabs.get(activeInfo.tabId, function(tabInfo) {
+		//error exception                                                                                                                                         
+		chrome.webRequest.onErrorOccurred.addListener(function(details) {
+			if (mediaPattern.test(details.url)) {
+			    console.log("failed to processing request " + details.url);
+			    if (!medialinksMap[details.url]) {
+				medialinks.push(details.url);
+			    }
+			}
+			return;
+		    },{urls: ["<all_urls>"]});
 
-//error exception
-chrome.webRequest.onErrorOccurred.addListener(function(details) {
-	if (mediaPattern.test(details.url)) {
-	    console.log("failed to processing request " + details.url);
-	    if (!medialinksMap[details.url]) {
-		medialinks.push(details.url);
-	    }
-	}
-	return;
-    },  
-    {urls: ["<all_urls>"]});
-
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	if(mediaPattern.test(details.url)) {
-	    //check repetition
-	    if (!medialinksMap[details.url]) {
-		console.log(details.url);
-		var vidList = document.getElementById("videoReq");
-		var li = document.createElement("li");
-		li.textContent = details.url;
-		vidList.appendChild(li);
-		medialinks.push(details.url);
-	    }
-	}
-	return;
-    },
-    {urls: ["<all_urls>"]});
-
-console.log("HI!!!!!!!");
-
-/*chrome.webRequest.onCompleted.addListener(function(details) {
-	console.log(JSON.stringify(details.url));
-      	return;
-    },
-    {urls: ["<all_urls>"]});*/
+		chrome.webRequest.onBeforeRequest.addListener(function(details) {
+			if(mediaPattern.test(details.url)) {
+			    //check repetition                                                                                                     
+			    if (!medialinksMap[details.url]) {
+				console.log(details.url);
+				var vidList = document.getElementById("videoReq");
+				var li = document.createElement("li");
+				li.textContent = details.url;
+				li.class = activeInfo.url;
+				vidList.appendChild(li);
+				medialinks.push(details.url);
+			    }
+			}
+			return;
+		    },{urls: ["<all_urls>"]});
+	    });
+    });
