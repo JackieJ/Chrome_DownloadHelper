4d06730b13057bd808b6ff03c66580db9dcc1524
Jackie Jin <liveaeon@gmail.com>
2012-07-03 15:47:07 -0700

    message connection failing

--- chrome_download_helper/webRequestGetter/mediaRequestProbe.js
+++ chrome_download_helper/webRequestGetter/mediaRequestProbe.js
@@ -25,58 +25,58 @@ var mediaRequestsMap = {
 //media file extensions regex
 var mediaPattern = /.*\.rmvb|.*\.mpg|.*\.mpeg|.*\.avi|.*\.rm|.*\.wmv|.*\.mov|.*\.flv|.*\.mpg3|.*\.mp4|.*\.mp3/;
 
-/*************Helper functions********************/
-// add timeout function for sanity checking
-var metaComparator = function(popupMeta, currentMeta) {
-    //compare the request num
-    if (popupMeta.requestsNum == currentMeta.requestNum) {
-	return false;
-    }
-    
-    
-    
-};
-
-
 //request listener from the popup
+
+var P;
+
 chrome.extension.onConnect.addListener(function(port) {
-    console.log("Request from popup detected!");
+    
+    console.dir(port);
+    
     if (port.name === "popup") {
-	
+	p = port;
     }                                                                                                         
 });          
 
 //list updater
-var listUpdater = function (tab) {
+var listUpdater = function (tab, isUpdated) {
+    
+    //change update status
+    mediaRequestsMap.update = isUpdated;
+    
     var hasTab = false;
     var tabMeta = mediaRequestsMap[tab.id];
+    
     if (tabMeta) {
 	hasTab = true;
     }
+    
     if (hasTab) {
 	var num = 0;
 	for (iter in tabMeta) {
 	    //skip the requestNum entry and iter through requests
-	    if(iter !== "requestNum" && tabMeta[iter].hasOwnProperty(requests)) {
+	    if(iter !== "requestNum") {
 		var requests = tabMeta[iter].requests;
 		for (i in requests) {
-		    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		    num++
 		}
 	    }
 	}
 	mediaRequestsMap[tab.id].requestNum = num;
     }
+    
+    if (P) {
+	P.postMessage(mediaRequestsMap);
+    }
+    
+    console.log(JSON.stringify(mediaRequestsMap));
 }
 
 var updateMeta = function(tab,tabId) {
     if (tab.active) {
 	activeURL = tab.url;
-	console.log("HELPER:"+activeURL);
-	console.log("HELPER-TABID:"+tabId+" "+ typeof tabId);
 	//update url activision state
 	//change the state of active url
-	console.log("=========CHANGE STATE=========");
 	var tabKey;
 	for (tabKey in mediaRequestsMap) {
 	    var urlKey;
@@ -88,16 +88,13 @@ var updateMeta = function(tab,tabId) {
 	    && mediaRequestsMap[tabId.toString()].hasOwnProperty(activeURL)) {
 	    (mediaRequestsMap[tabId.toString()])[activeURL].active = true;
 	}
-	console.log("META:"+JSON.stringify(mediaRequestsMap));
     }
     else {
-	console.log("non-active tab:"+tabId.toString());
 	if (mediaRequestsMap.hasOwnProperty(tabId.toString())
 	    && mediaRequestsMap[tabId.toString()].hasOwnProperty(tab.url)) {
 	    (mediaRequestsMap[tabId.toString()])[tab.url].active = false;
 	} 
     }
-    console.log("HELPER-METAUPDATE:"+JSON.stringify(mediaRequestsMap));
 };
 /************************************************/
 
@@ -105,15 +102,12 @@ var updateMeta = function(tab,tabId) {
 //add tab into the map
 chrome.tabs.onCreated.addListener(function(tab) {
     mediaRequestsMap[tab.id] = {requestNum:0};
-    console.log("onCreated Meta:"+JSON.stringify(mediaRequestsMap));
-    listUpdater(tab);
+    listUpdater(tab,false);
 });
 
 //remove tab from the map if user closes the tab, no access to the media url
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    console.log("onRemoved tabId:"+tabId);
     delete mediaRequestsMap[tabId];
-    console.log("onRemoved Meta:"+JSON.stringify(mediaRequestsMap));
 });
 
 //issue:when url changes, the tabid changes as well,
@@ -122,21 +116,16 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
     activeTabId = activeInfo.tabId;
     var tabID = activeInfo.tabId;
     chrome.tabs.get(tabID, function(tab) {
-	console.log("ONACTIVATED URL:"+tab.url);
 	activeURL = tab.url;
 	updateMeta(tab,tabID);
-	listUpdater(tab);
+	listUpdater(tab,false);
     });
-    console.log("ONACTIVATED tab id:"+tabID);
-    console.log("ONACTIVATED META:"+JSON.stringify(mediaRequestsMap));
-    
 });
 
 //update the active url info when the active tab completes updating
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log("ONUPDATED:updated tab id:"+tabId);
     updateMeta(tab,tabId);
-    listUpdater(tab);
+    listUpdater(tab,false);
 });
 
 //media request mapping
@@ -147,7 +136,6 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     if(mediaPattern.test(details.url)) {
 	//console.log("Inside webRequest:"+activeURL);
 	//console.log("Inside webRequest:"+details.tabId);
-	console.log("requests:"+details.url);
 	if(mediaRequestsMap.hasOwnProperty(details.tabId)) {
 	    //match tab with the tabId
 	    chrome.tabs.get(details.tabId, function(tab) {
@@ -155,8 +143,6 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 		if (!targetTab.hasOwnProperty(tab.url)) {
 		    targetTab[tab.url] = {};
 		}
-		console.log("webrequest tab url:"+tab.url);
-		console.log("webrequest active URL:"+activeURL);
 		//add request to the media meta
 		if (tab.url === activeURL) {
 		    //change url's activision state
@@ -174,9 +160,7 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 		var captureGroup = new RegExp("("+mediaPattern+")"+".*");
 		var mediaID = (captureGroup.exec(details.url))[1];
 		urlMeta.requests[mediaID] = details.url;
-		console.log("List Updator for WEBREUEST");
-		listUpdater(tab);
-		console.log("webrequest mediaTabAndUrlMeta:"+JSON.stringify(mediaRequestsMap));
+		listUpdater(tab,true);
 	    });
 	}
     }
--- chrome_download_helper/webRequestGetter/popup.js
+++ chrome_download_helper/webRequestGetter/popup.js
@@ -1,5 +1,13 @@
 //send request to the background page
-var mediaMeta = {
-    "update":false
-};
 
+var port = chrome.extension.connect({"name":"popup"});
+port.postMessage({"init":true});
+port.onMessage.addListener(function(msg) {
+    
+    //if(msg["update"] && msg["update"] === true) {
+	
+	console.log("updated!!");
+	
+    //
+    
+});
