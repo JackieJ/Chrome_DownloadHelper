b856168632155405f57773dc56a6546d0463178d
Jackie Jin <liveaeon@gmail.com>
2012-07-13 17:20:03 -0700

    ui construction finished, hooking up extension with nacl through hookupNACL()

--- chrome_download_helper/webRequestGetter/popup.js
+++ chrome_download_helper/webRequestGetter/popup.js
@@ -23,36 +23,94 @@ var metaComparator = function(newMeta) {
     return null;
 };
 
+var videoLink = null;
+var conversionType = null;
+
+var rMeta = {};
+
+var videoSelector = function(vID) {
+    
+    if(!videoLink) {
+	videoLink = null;
+    }
+    videoLink = rMeta[vID];
+    
+    //debugging message
+    console.log(JSON.stringify(videoLink));
+    
+};
+
+var hookupNACL = function(videoURL, convertType) {
+
+};
+
+var conversionTypeSelector = function(cType) {
+
+    if(!conversionType) {
+	conversionType = null;
+    }
+
+    //get conversion type
+    conversionType = cType;
+    
+    //send message to NACL and start encoding process
+    hookupNACL(videoLink, conversionType);    
+
+    //debugging message
+    console.log("conversion type:"+conversionType);
+    
+};
+
 var listContructor = function(requestsMeta) {
+    
+    rMeta = requestsMeta;
+    
+    var iter;
     for(iter in requestsMeta) { 
+	
 	var name = iter.length < 25 ? iter : iter.substring(0,25) + "...";
 	
-	var content = 
-	    "<li class=\"ui-li ui-li-static ui-body-c\" data-role=\"ui-bar-a\">"
-	    +"<a href="
-	    +requestsMeta[iter]+">"+name+"</a>"
-	    +"</li>";
+	var content = "<li onclick=\"videoSelector(\'"+iter+"\')\" "
+	+"class=\"ui-li ui-li-static ui-body-c\" data-role=\"ui-bar-a\">"
+	+"<a href=\"#conversionTypes\""
+	+">"+name+"</a></li>";
 	
 	$('#downloadList').append(content);
     }
 };
 
+//media type list
+var mediaTypes = [
+		  "MP3"
+		  ];
+
 $(document).ready(function() {
-    
-    var tabMeta;
-    var bgp = chrome.extension.getBackgroundPage();
-    tabMeta = metaComparator(bgp.mediaRequestsMap);
-    
-    var counterTag = document.getElementById("counter");
-    if(tabMeta) {
-	counterTag.textContent = tabMeta.requestNum;
-	for(reqMetaID in tabMeta) {
-	    var subMeta = tabMeta[reqMetaID];
-	    if(reqMetaID !== "requestNum") {
-		listContructor(subMeta.requests);
-	    }
+	
+	//construct the conversion type menu
+	for(var ID = 0; ID < mediaTypes.length; ID++) {
+	    
+	    var listItem = "<li class=\"ui-li ui-li-static ui-body-c\" data-role=\"ui-bar-a\""
+		+"onclick=\"conversionTypeSelector(\'"+mediaTypes[ID]+"\')\">"
+		+mediaTypes[ID]+"</li>";
+	    
+	    $('#types').append(listItem);
+
 	}
-    } else {
-	counterTag.textContent = 0;
-    }
-});
+	
+	var tabMeta;
+	var bgp = chrome.extension.getBackgroundPage();
+	tabMeta = metaComparator(bgp.mediaRequestsMap);
+	
+	var counterTag = document.getElementById("counter");
+	if(tabMeta) {
+	    counterTag.textContent = tabMeta.requestNum;
+	    for(reqMetaID in tabMeta) {
+		var subMeta = tabMeta[reqMetaID];
+		if(reqMetaID !== "requestNum") {
+		    listContructor(subMeta.requests);
+		}
+	    }
+	} else {
+	    counterTag.textContent = 0;
+	}
+    });
