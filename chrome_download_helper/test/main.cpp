fe254f36dde32e66ea029d54445198fbc735ee14
Jackie Jin <liveaeon@gmail.com>
2012-07-25 17:40:50 -0700

    [worked]test ffmpeg script

--- chrome_download_helper/test/main.cpp
+++ chrome_download_helper/test/main.cpp
@@ -1,30 +1,41 @@
-extern "C" { 
-  
-#include <libavcodec/avcodec.h>  
-#include "libavutil/mathematics.h"
-  
-}
-
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 
-using namespace
+using namespace std;
 
 #define INBUF_SIZE 4096
+#define MAX_BUF 999999
 
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Err:%s\n","Please specify the input file!");
+    exit(1);
+  }
   
-  const char* filename;
+  char* inputFileName = argv[1];
+  
+  //parse file into command line
+  printf ("%s\n","Checking if processor is available...");
+  if(system(NULL)) {
+    printf("%s\n","OK...");
+  } else {
+    exit(1);
+  }
+  
+  char eScript[MAX_BUF];
+  strcpy(eScript,"");
 
-  //enable avcodec lib
-  avcodec_init();
+  //script "ffmpeg -i input_file -strict experimental -sameq -s vga -aspect 1.7777  -y output.mp4";
 
-  //register codecs
-  avcodec_register_all();
-
+  strcat(eScript, "ffmpeg -i ");
+  strcat(eScript, inputFileName);
+  strcat(eScript, " -strict experimental -sameq -s vga -aspect 1.7777  -y output.mp4");
+  const char*executeScript = eScript;
+  printf("%s\n",executeScript);
   
+  system(executeScript);
   
-  
+  return 0;
 }
