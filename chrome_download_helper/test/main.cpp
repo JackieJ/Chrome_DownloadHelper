97a1218c84cf5cf9b05ce07f60488f63ecf07c9d
Jackie Jin <liveaeon@gmail.com>
2012-09-18 16:47:09 -0700

    attempt to build ffmpeg player from scratch

--- chrome_download_helper/test/main.cpp
+++ chrome_download_helper/test/main.cpp
@@ -1,49 +1,15 @@
-#include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 
 using namespace std;
 
-#define INBUF_SIZE 4096
-#define MAX_BUF 999999
-
-int main(int argc, char **argv) {
-  if (argc != 3) {
-    printf("Err:%s\n","Please specify the input file and the output type!");
-    exit(1);
-  }
+int main(int argc, char *argv[]) {
   
-  char* inputFileName = argv[1];
-  char* outputType = argv[2];
-  //parse file into command line
-  printf ("%s\n","Checking if processor is available...");
-  if(system(NULL)) {
-    printf("%s\n","OK...");
-  } else {
-    exit(1);
-  }
+  av_register_all();
   
-  char eScript[MAX_BUF];
-  strcpy(eScript,"");
-
-  //script "ffmpeg -i input_file -strict experimental -sameq -s vga -aspect 1.7777  -y output.mp4";
-
-  strcat(eScript, "ffmpeg -i ");
-  strcat(eScript, inputFileName);
-  strcat(eScript, " -strict experimental -sameq -aspect 1.7777  -y output");
-  
-  if (strcmp(outputType,"-mp3") == 0) {
-    strcat(eScript, ".mp3");
-  }
-  else if (strcmp(outputType, "-mp4") == 0) {
-    strcat(eScript, ".mp4");
-  }
-  
-  const char*executeScript = eScript;
-  printf("%s\n",executeScript);
-  
-  system(executeScript);
-  
+  printf("It works!\n");
   return 0;
 }
---
+++ chrome_download_helper/test/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+g++ main.cpp -lavformat -lavcodec -o main
+./main
